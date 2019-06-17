c3974efcea2cfc93c2b2df643276f5326d8903b8
Jeff Andrews <jeff@paintedbyjeff.com>
2019-06-17 13:09:37 -0400

    home 1300 061719
    added functions to disable the bet and roll buttons, which
    will guide player to do things in
    the proper order..

--- crownanchor.js
+++ crownanchor.js
@@ -1,11 +1,61 @@
 //this isn't going to be very DRY... baby steps, write it first, revise it later
 
-//start with 50 coins;
+// to do:
+
+
+
+// make bet status and round viewable
+// win if coins are over 150, loss if they reach zero
+
+// title page
+
+//setup
+wagerDefault();
+disableClick("rollButton");
 let purseCoins = 50;
 const dieArray = ['heartdie.png','crowndie.png','diamonddie.png','spadedie.png','anchordie.png','clubdie.png'];
-//let rollArray = [];
 let betArray = [];
 let betObject = {heart: 0, crown: 0, diamond: 0, spade: 0, anchor: 0, club: 0};
+let hornpipe;
+let round = 1;
+
+//start sound on page load
+function gameStart(){
+    hornpipe = new Sound('the-sailors-hornpipe.mp3');
+    hornpipe.play();
+}
+
+
+//sound class
+class Sound {
+    constructor(src) {
+        this.sound = document.createElement("audio");
+        this.sound.src = src;
+        this.sound.setAttribute("preload", "auto");
+        this.sound.setAttribute("loop", "true");
+        this.sound.setAttribute("controls", "none");
+        this.sound.style.display = "none";
+        document.body.appendChild(this.sound);
+        this.play = function () {
+            this.sound.play();
+        };
+        this.stop = function () {
+            this.sound.pause();
+        };
+    }
+}
+
+//disable click
+function disableClick (elementId) {
+    let x = document.getElementById(elementId);
+    x.disabled = true;
+    }
+
+//enable click
+function enableClick (elementId) {
+    let x = document.getElementById(elementId);
+    x.disabled = false;
+    }
 
 
 //resets all input values to 0
@@ -14,10 +64,15 @@ function wagerDefault() {
     for(i=0; i<x.length; i++)
     x[i].value = "0";
   }
-
+//updates html value for coins
 function updateCoins() {
     let x = document.getElementById("coins");
     x.innerHTML = `Coins: ${purseCoins}`;
+}
+//updates round!
+function updateRound() {
+    let x = document.getElementById("round");
+    x.innerHTML = `Round ${round++}`;
 }
 
 function resetValues() {
@@ -27,6 +82,7 @@ function resetValues() {
     updateCoins();
 }
 
+updateRound();
 updateCoins();
 //
 function placeBet() {
@@ -70,12 +126,11 @@ function placeBet() {
             alert(`you now have ${purseCoins} coins in your purse`);
         } */
         updateCoins();
+        disableClick("betButton");
+        enableClick("rollButton");
 }
         
-
-//if there's a bet placed on a square, and that square corresponds with any of the die, multiply the number of die
-//corresponding with the amount on the square.
-
+//roll die and add point for matches
 function rollDie() {
     let roundPoints = 0;
     for(i=1; i<4; i++){
@@ -90,8 +145,11 @@ function rollDie() {
     //console.log(`you won ${roundPoints} points this round`);
     purseCoins = purseCoins + roundPoints;
     //console.log(`you now have ${purseCoins} coins in your purse!`);
-    document.getElementsByClassName("wager").value = "0";
+    //document.getElementsByClassName("wager").value = "0";
     //console.log(betArray);
     resetValues();
     betArray=[];
+    updateRound();
+    disableClick("rollButton");
+    enableClick("betButton");
 }
