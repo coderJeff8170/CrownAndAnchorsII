20f40d6360812e72b38982352114451451ce8093
Jeff Andrews <jeff@paintedbyjeff.com>
2019-06-18 18:45:58 -0400

    work
    neatened up the code for readability,
    added preventions for no wager,
    and functions to alert wins and losses
    and subsequently restart the game.

--- crownanchor.js
+++ crownanchor.js
@@ -1,7 +1,8 @@
 // to do:
-
-// make bet status and round viewable
-// win if coins are over 150, loss if they reach zero
+// sounds
+// add rules
+// make bet status and round viewable or at least
+// alert matches..
 
 // title page
 
@@ -22,7 +23,6 @@ function gameStart(){
     hornpipe = new Sound('the-sailors-hornpipe.mp3');
     hornpipe.play();
 }
-
 
 //sound class
 class Sound {
@@ -69,57 +69,72 @@ function updateRound() {
     let x = document.getElementById("round");
     x.innerHTML = `Round ${round++}`;
 }
+//resets values for end of each round.
 function resetValues() {
     betObject = {heart: 0, crown: 0, diamond: 0, spade: 0, anchor: 0, club: 0};
     //betArray = [0, 0, 0, 0, 0, 0];
     wagerDefault();
     updateCoins();
 }
-
-
+//loss function
+function youLose() {
+    const loss = "Game Over! You have no more coins. Hit ok to play again.";
+    alert (loss);
+    location.reload();
+  }
+//win function
+  function youWin() {
+    const win = "You have 150 coins! You Won! Hit ok to play again.";
+    alert (win);
+    location.reload();
+  }
+//bet function
 function placeBet() {
-        betObject.heart = document.getElementById('heart').valueAsNumber;
-        betObject.crown = document.getElementById('crown').valueAsNumber;
-        betObject.diamond = document.getElementById('diamond').valueAsNumber;
-        betObject.spade = document.getElementById('spade').valueAsNumber;
-        betObject.anchor = document.getElementById('anchor').valueAsNumber;
-        betObject.club = document.getElementById('club').valueAsNumber;
-        betArray = Object.values(betObject);
+    betObject.heart = document.getElementById('heart').valueAsNumber;
+    betObject.crown = document.getElementById('crown').valueAsNumber;
+    betObject.diamond = document.getElementById('diamond').valueAsNumber;
+    betObject.spade = document.getElementById('spade').valueAsNumber;
+    betObject.anchor = document.getElementById('anchor').valueAsNumber;
+    betObject.club = document.getElementById('club').valueAsNumber;
+    betArray = Object.values(betObject);
 
-        //if player enters a negative bet value, 
-        for(i=0; i<betArray.length; i++) {
-            if(betArray[i]<0){
-                alert(`Negative Bet Invalid!
-                Please try again!`);
-                wagerDefault();
-                return;
-            }
-        }
-
-        console.log(betArray);//yay!
-        console.log(purseCoins);
-
-        //total up the array of bets
-        let currentWager = betArray.reduce((a, x) => a += x);
-        //subtract current wager from the purse
-        purseCoins = purseCoins - currentWager;
-        console.log(purseCoins);
-
-        if(purseCoins < 0) {
-            alert(`you don't have enough coins to place this bet!
+    //prevent negative bet values 
+    for(i=0; i<betArray.length; i++) {
+        if(betArray[i]<0){
+            alert(`Negative Bet Invalid!
             Please try again!`);
-            purseCoins = purseCoins + currentWager;
             wagerDefault();
             return;
-         } 
-         /*else if(purseCoins=0) {
-            alert(`your purse is empty!`);
-        }else{
-            alert(`you now have ${purseCoins} coins in your purse`);
-        } */
-        updateCoins();
-        disableClick("betButton");
-        enableClick("rollButton");
+        }
+    }
+
+    //total up the array of bets
+    let currentWager = betArray.reduce((a, x) => a += x);
+
+    //prevent a wager of 0
+    if(currentWager === 0) {
+        alert(`you must bet something!
+        Please try again!`);
+        wagerDefault();
+        return;
+    }
+
+    //subtract current wager from the purse
+    purseCoins = purseCoins - currentWager;
+    console.log(purseCoins);
+
+    //prevent unaffordable wager
+    if(purseCoins < 0) {
+        alert(`you don't have enough coins to place this bet!
+        Please try again!`);
+        purseCoins = purseCoins + currentWager;
+        wagerDefault();
+        return;
+        } 
+
+    updateCoins();
+    disableClick("betButton");
+    enableClick("rollButton");
 }
         
 //roll die and add point for matches
@@ -134,14 +149,30 @@ function rollDie() {
                 }
         }
     }
-    //console.log(`you won ${roundPoints} points this round`);
+    //delay alerts for dice focus
+    setTimeout(() => {
+            //message wins/losses - improve
+    if(roundPoints>0) {
+        alert(`Ahoy, Sailor! Ye won a total of ${roundPoints} coins this round!`);
+    }else{
+        alert(`Shiver me timbers! Ye didn't win any coins this round.`);
+    }
+
+    //add winnings to purse
     purseCoins = purseCoins + roundPoints;
-    //console.log(`you now have ${purseCoins} coins in your purse!`);
-    //document.getElementsByClassName("wager").value = "0";
-    //console.log(betArray);
-    resetValues();
-    betArray=[];
-    updateRound();
-    disableClick("rollButton");
-    enableClick("betButton");
+
+    //win/loss/continue
+    if(purseCoins === 0) {
+        youLose();
+    }else if(purseCoins === 150) {
+        youWin();
+    }else{
+        resetValues();
+        betArray=[];
+        updateRound();
+        disableClick("rollButton");
+        enableClick("betButton");
+    }
+    }, 1*1000);
+
 }
