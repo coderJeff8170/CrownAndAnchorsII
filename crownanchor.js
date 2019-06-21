adb801ce6ddf20088fa9147a413bb66788723f6c
Jeff Andrews <jeff@paintedbyjeff.com>
2019-06-21 18:11:34 -0400

    new branch created for trying  to add modal windows and figure out how to have the document reload after the modal is clicked off promise, or event listener??

--- crownanchor.js
+++ crownanchor.js
@@ -1,6 +1,7 @@
 // to do:
 // sounds
 // add rules
+//create modals instead of alert boxes
 // make bet status and round viewable or at least
 // alert matches..
 
@@ -76,11 +77,51 @@ function resetValues() {
     wagerDefault();
     updateCoins();
 }
+
+//MODAL STUFF
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+//var btn = document.getElementById("myBtn");
+
+//Get the modal text area
+var modalMessage = document.getElementById("modalText");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal  REPLACE BUTTON CLICK WITH CONDITION;
+function displayModal(message) {
+  modal.style.display = "block";
+  modalMessage.innerHTML = message;
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
 //loss function
 function youLose() {
-    const loss = "Game Over! You have no more coins. Hit ok to play again.";
-    alert (loss);
-    location.reload();
+    const loss = "Game Over! You have no more coins. Close window to play again.";
+    displayModal(loss);
+    if(modal.style.display = "none") {
+        location.reload();
+    }
+    //shit. do I have to use a promise here??
+    //alert (loss);
+    //location.reload(); ---> OK, so this is the problem for the modal - must occur after the modal closed
+    //and not happen on any other modal except the YOU WIN Modal....
   }
 //win function
   function youWin() {
